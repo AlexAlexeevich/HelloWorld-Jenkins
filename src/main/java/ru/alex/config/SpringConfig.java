258970fcad6b08a27729cc6f91b55822ccd0ee9a
Alex Baranov <baranov.alexalex@gmail.com>
2023-08-27 22:08:55 +0300

    first commit

---
+++ src/main/java/ru/alex/config/SpringConfig.java
@@ -0,0 +1,69 @@
+package ru.alex.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("ru.alex")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+    private final ApplicationContext applicationContext;
+    private final Environment environment;
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
+        this.applicationContext = applicationContext;
+        this.environment = environment;
+    }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//
+//        dataSource.setDriverClassName(environment.getRequiredProperty("hibernate.driver_class"));
+//        dataSource.setUrl(environment.getRequiredProperty("hibernate.connection.url"));
+//        dataSource.setUsername(environment.getRequiredProperty("hibernate.connection.username"));
+//        dataSource.setPassword(environment.getRequiredProperty("hibernate.connection.password"));
+//
+//        return dataSource;
+//    }
+//
+//    private Properties hibernateProperties() {
+//        Properties properties = new Properties();
+//        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+//        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+//
+//        return properties;
+//    }
+
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource());
+//        sessionFactory.setPackagesToScan("ru.alex.entity");
+//        sessionFactory.setHibernateProperties(hibernateProperties());
+//
+//        return sessionFactory;
+//    }
+
+//    @Bean
+//    public PlatformTransactionManager hibernateTransactionManager() {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(sessionFactory().getObject());
+//
+//        return transactionManager;
+//    }
+//
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer
+//                .favorPathExtension(true)
+//                .ignoreAcceptHeader(true)
+//                .defaultContentType(MediaType.APPLICATION_JSON);
+//    }
+}
